7753d29e655de8c2f8a566b84cf952c19e8fcf6b
Zoltan C. Toth <tz@looper.hu>
2015-11-13 08:36:04 +0100

    example tables added

---
+++ create_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE birdstrikes (
+    id INTEGER NOT NULL,
+    aircraft VARCHAR(32),
+    flight_date DATE NOT NULL,
+    damage VARCHAR(16) NOT NULL,
+    airline VARCHAR(255) NOT NULL,
+    state VARCHAR(255),
+    phase_of_flight VARCHAR(32) NOT NULL,
+    reported_date DATE,
+    bird_size VARCHAR(16),
+    cost INTEGER NOT NULL,
+    speed INTEGER,
+    PRIMARY KEY(id)
+);
+
